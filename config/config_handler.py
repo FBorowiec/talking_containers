379891a4a69bb5cb6d4cafb3524c884e9bde8296
Franek Borowiec <franek.borowiec@gmail.com>
2021-07-18 18:13:56 +0200

    Use yaml settings file

--- config/config_handler.py
+++ config/config_handler.py
@@ -1,10 +1,11 @@
 # pylint: disable=too-few-public-methods
-from configparser import ConfigParser
 from os import path
+import yaml
 
 
 class SettingsHandler:
+    handler = None
     current_directory = path.dirname(path.realpath(__file__))
-    file_path = path.normpath(path.join(current_directory, "settings.ini"))
-    handler = ConfigParser()
-    handler.read(file_path)
+    file_path = path.normpath(path.join(current_directory, "settings.yaml"))
+    with open(file_path, "r") as f:
+        handler = yaml.safe_load(f)
